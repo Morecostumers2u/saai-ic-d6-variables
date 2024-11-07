439ca3f3c54f3b2e82bb06442b7dba04a66d4cdd
Morecostumers2u <javierayala.ca@gmail.com>
2024-11-07 03:36:35 +0000

    variables use

---
+++ script.js
@@ -0,0 +1,12 @@
+let favoriteFood ="tacos"
+console.log(favoriteFood)
+favoriteFood= "wingstop"
+console.log(favoriteFood)
+
+const favoriteHoliday ="thanksgiving"
+console.log(favoriteHoliday)
+
+console.log("favoriteFood", favoriteFood)
+console.log("favoriteHoliday")
+
+
